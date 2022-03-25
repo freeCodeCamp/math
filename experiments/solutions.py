eee20a789894eeba1a699b9f1b83ca10f2970f79
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-03-25 16:46:35 +0100

    feat: add more steps and a solution file

---
+++ experiments/solutions.py
@@ -0,0 +1,9 @@
+solutions = [
+    """var = 'demo'
+runner.run(tests.step01)
+""",
+    """a = 'ay'
+b = 'bee'
+runner.run(tests.step02)
+""",
+]
--- experiments/tests.py
+++ experiments/tests.py
@@ -1,10 +1,23 @@
 from user_env import singleton as user_env
 import re
 
+
 def step01():
-    assert user_env.getVariable("var") == "demo", "var should contain the 'demo' string!"
-    assert re.search("'\w*'", user_env.getCode()), 'single quotes please!'
+    assert (
+        user_env.getVariable("var") == "demo"
+    ), "var should contain the 'demo' string!"
+    assert re.search("'\w*'", user_env.getCode()), "single quotes please!"
     print("Step 01: Success!")
+
+
+def step02():
+    assert (
+        user_env.getVariable("a") == "ay"
+    ), "variable a should contain the 'ay' string!"
+    assert (
+        user_env.getVariable("b") == "bee"
+    ), "variable b should contain the 'bee' string!"
+    print("Step 02: Success!")
 
 
 def prepare(user_globals, user_input):
